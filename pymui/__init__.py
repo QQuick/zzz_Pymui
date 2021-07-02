61e209531522781b0b00eecf5bb5dcc8577701ff
JdeH <info@geatec.com>
2021-07-03 00:30:28 +0200

    New components added

--- pymui/__init__.py
+++ pymui/__init__.py
@@ -1,10 +1,15 @@
 MaterialUI = require ('@material-ui/core')
 MaterialUI_lab = require ('@material-ui/lab')
 
+Box = MaterialUI.Box
 Button = MaterialUI.Button
 AppBar = MaterialUI.AppBar
 Tabs = MaterialUI.Tabs
 Tab = MaterialUI.Tab
+Grid = MaterialUI.Grid
+Paper = MaterialUI.Paper
+Card = MaterialUI.Card
+
 TabPanel = MaterialUI_lab.TabPanel
 TabList = MaterialUI_lab.TabList
 TabContext = MaterialUI_lab.TabContext
