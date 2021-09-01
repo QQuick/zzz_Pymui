38e99f2f9203c7ed121deb0eb3fcb5026ffd9425
JdeH <info@geatec.com>
2021-09-01 18:49:26 +0200

    OO-ized

--- pymui/__init__.py
+++ pymui/__init__.py
@@ -1,16 +1,36 @@
+import pyact as pa
+
 MaterialUI = require ('@material-ui/core')
 MaterialUI_lab = require ('@material-ui/lab')
 
-AppBar = MaterialUI.AppBar
-Box = MaterialUI.Box
-Button = MaterialUI.Button
-Card = MaterialUI.Card
-Grid = MaterialUI.Grid
-Paper = MaterialUI.Paper
-Slider = MaterialUI.Slider
-Tab = MaterialUI.Tab
-Tabs = MaterialUI.Tabs
+class Wrapper:
+    def __init__ (self, Wrappee, args):
+        self.Wrappee = Wrappee
+        self.args = args
 
-TabContext = MaterialUI_lab.TabContext
-TabList = MaterialUI_lab.TabList
-TabPanel = MaterialUI_lab.TabPanel
+    def el (self):
+        return pa.createElement (self.Wrappee, *self.args)
+
+AppBar = lambda *args: Wrapper (MaterialUI.AppBar, args)
+Box = lambda *args: Wrapper (MaterialUI.Box, args)
+Button = lambda *args: Wrapper (MaterialUI.Button, args)
+Card = lambda *args: Wrapper (MaterialUI.Card, args)
+Grid = lambda *args: Wrapper (MaterialUI.Grid, args)
+Paper = lambda *args: Wrapper (MaterialUI.Paper, args)
+Slider = lambda *args: Wrapper (MaterialUI.Slider, args)
+Tab = lambda *args: Wrapper (MaterialUI.Tab, args)
+Tabs = lambda *args: Wrapper (MaterialUI.Tabs, args)
+TextField = lambda *args: Wrapper (MaterialUI.TextField, args)
+
+TabContext = lambda *args: Wrapper (MaterialUI_lab.TabContext, args)
+TabList = lambda *args: Wrapper (MaterialUI_lab.TabList, args)
+TabPanel = lambda *args: Wrapper (MaterialUI_lab.TabPanel, args)
+
+Canvas = lambda *args: Wrapper ('canvas', args)
+
+class String:
+    def __init__ (self, Wrappee):
+        self.Wrappee = Wrappee
+
+    def el (self):
+        return self.Wrappee
